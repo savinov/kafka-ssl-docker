093e1b795d77131fb36864fb0503608ca029e92c
Guram Savinov <guram.savinov@gmail.com>
2021-07-14 15:03:51 +0300

    1. docker-cp.sh: copy *.jks files to jks directory 2. add jks directory to .gitignore

--- docker-cp.sh
+++ docker-cp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker cp kafkassl:/kafka_2.12-2.5.0/ssl/server.keystore.jks server.keystore.jks
-docker cp kafkassl:/kafka_2.12-2.5.0/ssl/server.truststore.jks server.truststore.jks
-docker cp kafkassl:/kafka_2.12-2.5.0/ssl/client.keystore.jks client.keystore.jks
-docker cp kafkassl:/kafka_2.12-2.5.0/ssl/client.truststore.jks client.truststore.jks
+mkdir -p jks
+
+docker cp kafkassl:/kafka_2.12-2.5.0/ssl/server.keystore.jks jks/server.keystore.jks
+docker cp kafkassl:/kafka_2.12-2.5.0/ssl/server.truststore.jks jks/server.truststore.jks
+docker cp kafkassl:/kafka_2.12-2.5.0/ssl/client.keystore.jks jks/client.keystore.jks
+docker cp kafkassl:/kafka_2.12-2.5.0/ssl/client.truststore.jks jks/client.truststore.jks
